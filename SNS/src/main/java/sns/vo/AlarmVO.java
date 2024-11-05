1b56bdad4bdb75a4a6939fd4784fce1696afd90f
ParkInJae <pak293100@jbnu.ac.kr>
2024-11-05 17:06:36 +0900

    add

---
+++ SNS/src/main/java/sns/vo/AlarmVO.java
@@ -0,0 +1,30 @@
+package sns.vo;
+
+public class AlarmVO {
+	private int alno ;
+	private int uno ;
+	private String rdate ;
+	private String state ;
+	private String type ;
+	private int no ;
+	
+	public int getAlno() {return alno;}
+	public int getUno() {return uno;}
+	public String getRdate() {return rdate;}
+	public String getState() {return state;}
+	public String getType() {return type;}
+	public int getNo() {return no;}
+	
+	
+	
+	public void setAlno(int alno) {this.alno = alno;}
+	public void setUno(int uno) {this.uno = uno;}
+	public void setRdate(String rdate) {this.rdate = rdate;}
+	public void setState(String state) {this.state = state;}
+	public void setType(String type) {this.type = type;}
+	public void setNo(int no) {this.no = no;}
+
+	
+	
+	
+}
